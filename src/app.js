8baf27de01c64113c33a1f296453375839893e79
Wasiu Bakare <wasiubakare@gmail.com>
2025-02-03 17:15:42 +0100

    feat: add a custom welcome message to / route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "Welcome to the Number Classification API! Use /api/classify-number?number=<your_number> to classify a number.",
+  });
+});
 app.get("/api/classify-number", classifyNumber);
 
 module.exports = app;
